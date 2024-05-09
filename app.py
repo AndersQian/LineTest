78574418bee700487f8cb887d86b55da4aa5a4a3
AndersQian <yh253614@winland.com.tw>
2024-05-09 23:29:29 +0800

    init

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def welcome(event):
     gid = event.source.group_id
     profile = line_bot_api.get_group_member_profile(gid, uid)
     name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
+    message = TextSendMessage(text=f'{name}歡迎加入AAAAAAAA')
     line_bot_api.reply_message(event.reply_token, message)
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
